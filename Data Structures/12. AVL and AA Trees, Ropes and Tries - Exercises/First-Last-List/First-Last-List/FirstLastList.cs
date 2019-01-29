9577b888ca9c83cfd26c191dc526de55b3331b88
unknown <kriskobota@abv.bg>
2019-01-29 22:50:08 +0200

    Add exercises for AVL, AA Trees, Ropes and Tries

---
+++ Data Structures/12. AVL and AA Trees, Ropes and Tries - Exercises/First-Last-List/First-Last-List/FirstLastList.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class FirstLastList<T> : IFirstLastList<T> where T : IComparable<T>
+{
+    private LinkedList<T> insertedValues;
+    private OrderedBag<LinkedListNode<T>> ascendingElements;
+    private OrderedBag<LinkedListNode<T>> descendingElement;
+  
+
+    public FirstLastList()
+    {
+        this.insertedValues = new LinkedList<T>();
+        this.ascendingElements = new OrderedBag<LinkedListNode<T>>((x,y) => x.Value.CompareTo(y.Value));
+        this.descendingElement = new OrderedBag<LinkedListNode<T>>((x,y) => y.Value.CompareTo(x.Value));
+    }
+
+    public int Count
+    {
+        get
+        {
+            return this.insertedValues.Count;
+        }
+    }
+
+    public void Add(T element)
+    {
+        LinkedListNode<T> node = new LinkedListNode<T>(element);
+        this.insertedValues.AddLast(node);
+        this.ascendingElements.Add(node);
+        this.descendingElement.Add(node);
+    }
+
+    public void Clear()
+    {
+        this.insertedValues.Clear();
+        this.ascendingElements.Clear();
+        this.descendingElement.Clear();
+    }
+
+    public IEnumerable<T> First(int count)
+    {
+
+        IsInBounds(count);
+        LinkedListNode<T> current = this.insertedValues.First;
+        int iteration = 0;
+
+        while (iteration < count)
+        {
+            yield return current.Value;
+            current = current.Next;
+            iteration++;
+        }
+    }
+
+    private void IsInBounds(int count)
+    {
+        if (this.insertedValues.Count < count)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    }
+
+    public IEnumerable<T> Last(int count)
+    {
+        this.IsInBounds(count);
+        LinkedListNode<T> current = this.insertedValues.Last;
+        int iteration = 0;
+
+        while(iteration < count)
+        {
+            yield return current.Value;
+            current = current.Previous;
+            iteration++;
+        }
+    }
+
+    public IEnumerable<T> Max(int count)
+    {
+        this.IsInBounds(count);
+        return this.descendingElement.Take(count).Select(x => x.Value);
+    }
+
+    public IEnumerable<T> Min(int count)
+    {
+        this.IsInBounds(count);
+        return this.ascendingElements.Take(count).Select(x => x.Value);
+    }
+
+    public int RemoveAll(T element)
+    {
+        LinkedListNode<T> node = new LinkedListNode<T>(element);
+
+        foreach (var item in this.ascendingElements.Range(node, true, node, true))
+        {
+            this.insertedValues.Remove(item);
+        }
+
+        int count = this.ascendingElements.RemoveAllCopies(node);
+        this.descendingElement.RemoveAllCopies(node);
+
+        return count;
+    }
+}
---
+++ Data Structures/12. AVL and AA Trees, Ropes and Tries - Exercises/First-Last-List/First-Last-List/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        FirstLastList<int> list = new FirstLastList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        var result = list.First(3);
+        Console.WriteLine();
+    }
+}
